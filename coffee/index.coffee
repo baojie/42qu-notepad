1662a8c67d90adc1c3cfa96d10b14063f63fb72b
用户名 <>
2012-09-28 15:37:40 +0000

    f

--- coffee/index.coffee
+++ coffee/index.coffee
@@ -1,31 +1,30 @@
-$ ->
-    txt = $ '#textarea'
-    date = new Date()
-    posted = true
-    timer = 0
-    txt_val = ''
-    post = ->
-        if posted && (txt_val!=txt.val())
-            posted = false
-            $.post(
-                location.href,
-                {txt: txt.val()},
-                ->
-                    #$('.save_hint').html("#{date.toLocaleTimeString()} 保存成功!")
-                    posted = true 
-                    txt_val = txt.val()
-            )
-        timer && clearTimeout timer
-        timer = setTimeout(post,3000)
-
-    key = ->
+txt = $ '#txt'
+date = new Date()
+posted = true
+timer = 0
+txt_val = ''
+post = ->
+    if ! posted && (txt_val!=txt.val())
         posted = false
-        return 1
+        $.post(
+            location.href,
+            {txt: txt.val()},
+            ->
+                #$('.save_hint').html("#{date.toLocaleTimeString()} 保存成功!")
+                posted = true 
+                txt_val = txt.val()
+        )
+    timer && clearTimeout timer
+    timer = setTimeout(post,3000)
 
-    txt.keydown(key)       
-    txt.keyup(key)
+key = ->
+    posted = false
+    return 1
 
-    post()
-    focus txt[0]
+txt.keydown(key)       
+txt.keyup(key)
+
+post()
+focus txt[0]
 
 
--- static/js/index.js
+++ static/js/index.js
@@ -1,33 +1,41 @@
 (function() {
+  var date, key, post, posted, timer, txt, txt_val;
 
-  $(function() {
-    var date, key, post, posted, timer, txt, txt_val;
-    txt = $('#textarea');
-    date = new Date();
-    posted = true;
-    timer = 0;
-    txt_val = '';
-    post = function() {
-      if (posted && (txt_val !== txt.val())) {
-        posted = false;
-        $.post(location.href, {
-          txt: txt.val()
-        }, function() {
-          posted = true;
-          return txt_val = txt.val();
-        });
-      }
-      timer && clearTimeout(timer);
-      return timer = setTimeout(post, 3000);
-    };
-    key = function() {
+  txt = $('#txt');
+
+  date = new Date();
+
+  posted = true;
+
+  timer = 0;
+
+  txt_val = '';
+
+  post = function() {
+    if (!posted && (txt_val !== txt.val())) {
       posted = false;
-      return 1;
-    };
-    txt.keydown(key);
-    txt.keyup(key);
-    post();
-    return focus(txt[0]);
-  });
+      $.post(location.href, {
+        txt: txt.val()
+      }, function() {
+        posted = true;
+        return txt_val = txt.val();
+      });
+    }
+    timer && clearTimeout(timer);
+    return timer = setTimeout(post, 3000);
+  };
+
+  key = function() {
+    posted = false;
+    return 1;
+  };
+
+  txt.keydown(key);
+
+  txt.keyup(key);
+
+  post();
+
+  focus(txt[0]);
 
 }).call(this);
