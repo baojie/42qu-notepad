0524d34fb1aa42af46b092a8b05e0e407e7383ff
用户名 <>
2012-10-27 12:42:56 +0000

    f

---
+++ js/_hash_.py
@@ -0,0 +1,25 @@
+#coding:utf-8
+
+import _env
+
+__HASH__ =  {
+    "base.js" : 'xVbfwK4lLRAPjYMx5ldWmg.js', #base
+    "index.js" : 'MVD361eEnY8QtCDZNSCOzw.js', #index
+}
+
+
+from config import DEBUG, HOST, HOST_CSS_JS
+from os.path import dirname,basename
+__vars__ = vars()
+
+for file_name, hash in __HASH__.iteritems():
+    
+    if DEBUG:
+        value = "http://%s/%s/%s"%(HOST, basename(dirname(__file__)),   file_name)
+    else:
+        value = "http://%s/build/%s"%(HOST_CSS_JS, hash) 
+    
+    name = file_name.rsplit('.', 1)[0].replace('.', '_').replace('-', '_').replace('/', '_')
+    print      "http://%s/build/%s"%(HOST_CSS_JS, hash) 
+    __vars__[name] = value
+                            
