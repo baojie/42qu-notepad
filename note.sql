b372951253812e21e6d490d8702ec505baea40d1
lerry <lvdachao@gmail.com>
2012-10-27 13:58:33 +0800

    fix

--- note.sql
+++ note.sql
@@ -1,7 +1,8 @@
+DROP DATABASE IF EXISTS `work_notepad`;
 CREATE DATABASE work_notepad;
 DROP TABLE IF EXISTS `work_notepad`.`notepad`;
 CREATE TABLE  `work_notepad`.`notepad` (
-  `url` char(1) COLLATE utf8_bin NOT NULL,
+  `url` char(9) COLLATE utf8_bin NOT NULL,
   `txt` mediumtext COLLATE utf8_bin NOT NULL,
   `time` bigint(20) unsigned NOT NULL,
   PRIMARY KEY (`url`)
