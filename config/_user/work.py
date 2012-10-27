fc09525e13929cf8c4a6888c9dc959d9769311bf
用户名 <>
2012-10-27 03:54:11 +0000

    f

--- config/_user/work.py
+++ config/_user/work.py
@@ -1,11 +1,8 @@
 #coding:utf-8
 import _env
-from misc.config._private.upyun import pre_config as pre_config_upyun 
-from misc.config._private.pay import pre_config as pre_config_pay
 
 def pre_config(o):
-    pre_config_upyun(o)
-    pre_config_pay(o)
+    pass
 
 def post_config(o):
     pass
