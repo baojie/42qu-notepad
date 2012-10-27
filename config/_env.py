2f123f5ac0adde8d5fa6223929cd8f7c9b48f391
用户名 <>
2012-10-27 10:50:26 +0000

    f

--- config/_env.py
+++ config/_env.py
@@ -1,1 +1,23 @@
-../_env.py
+#coding:utf-8
+
+import sys
+from os.path import dirname, abspath, exists, join
+if sys.getdefaultencoding() != 'utf-8':
+    reload(sys)
+    sys.setdefaultencoding('utf-8')
+
+
+PREFIX = dirname(abspath(__file__))
+def _():
+    global PREFIX 
+    PWD = abspath(__file__)
+    while True and len(PWD) > 1:
+        PWD = dirname(PWD)
+        if exists('%s/model'%PWD) and exists('%s/lib'%PWD) and exists('%s/view'%PWD):
+            PREFIX = PWD
+    if PREFIX and PREFIX not in sys.path:
+        sys.path.insert(0, PREFIX)
+
+
+
+_()
