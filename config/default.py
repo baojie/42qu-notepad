f4a558ae0325723e73e214b57ad90137dc848798
用户名 <>
2012-10-27 04:37:35 +0000

    f

--- config/default.py
+++ config/default.py
@@ -3,28 +3,6 @@ import sys
 reload(sys)
 sys.setdefaultencoding('utf-8')
 
-# 初始化 mako 模版 的 查找
-from os.path import dirname, abspath, exists, join
-from mako.lookup import TemplateLookup
-
-
-DEBUG = True
-PREFIX = dirname(dirname(abspath(__file__)))
-
-_lookup = TemplateLookup(
-    directories=join(PREFIX, 'html'),
-    module_directory='/tmp/mako',
-    disable_unicode=True,
-    encoding_errors='ignore',
-    default_filters=['str', 'h'],
-    filesystem_checks=DEBUG,
-    input_encoding='utf-8',
-    output_encoding=''
-)
-
-
-def render(html, **kwds):
-    return _lookup.get_template(html).render(**kwds)
 
 
 
