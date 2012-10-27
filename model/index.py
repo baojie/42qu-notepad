818bdec07d3ad996b9e7bd5be447a4a5c14ca26b
lerry <lvdachao@gmail.com>
2012-10-27 17:57:03 +0800

    fix

--- model/index.py
+++ model/index.py
@@ -4,10 +4,16 @@ import _env
 import time
 from random import choice
 from _db import connection
+try:
+    from sae.kvdb import KVClient as kv
+except:
+    from kvstore import kv
+    
+    
 
 URL_ENCODE = 'abcdefghijklmnopqrstuvwxyz0123456789'
 
-def txt_by_url(url):
+def _txt_by_url(url):
     url = url.lower()
     cursor = connection.cursor()
     cursor.execute('select txt from notepad where url=%s', url)
@@ -18,6 +24,11 @@ def txt_by_url(url):
         txt = ''
     return txt
 
+def txt_by_url(url):
+    url = str(url.lower())
+    db = kv()
+    return db.get(url) or ''
+
 def gen_url():
     while True:
         url = ''.join(choice(URL_ENCODE) for i in xrange(9))
@@ -26,7 +37,7 @@ def gen_url():
     return url
 
 
-def save_txt(url, txt):
+def _save_txt(url, txt):
     now = int(time.time())
     cursor = connection.cursor()
     if txt:
@@ -37,3 +48,11 @@ def save_txt(url, txt):
         )
     else:
         cursor.execute('delete from notepad where url=%s', url)
+
+def save_txt(url, txt):
+    url = str(url.lower())
+    db = kv()
+    if txt:
+        db.set(url, txt)
+    else:
+        db.set(url, '')
