d1e315c5be0d43ec38e80f17d82e5598a412042d
用户名 <>
2012-10-27 03:49:06 +0000

    f

--- model/_db.py
+++ model/_db.py
@@ -4,11 +4,13 @@
 # 初始化数据连接
 import MySQLdb
 from DBUtils.PersistentDB  import PersistentDB as DB
-
+from config import MYSQL_HOST, MYSQL_PORT, MYSQL_USER, MYSQL_PASSWD
 def _connection(*args, **kwds):
     kwds['maxusage'] = False
     persist = DB (MySQLdb, *args, **kwds)
     conn = persist.connection()
     return conn
 
-connection = _connection(host='127.0.0.1', user='root', passwd='42qu', db='work_notepad')
+connection = _connection(
+    host=MYSQL_HOST, user=MYSQL_USER, passwd=MYSQL_PASSWD, db=MYSQL_PORT
+)
