34e1848a2e2b4e29c8de64ec57ab95f29730d581
用户名 <>
2012-10-27 11:01:10 +0000

    f

--- view/index.py
+++ view/index.py
@@ -8,23 +8,8 @@ from _view import View, login
 from model._db import connection
 from model.index import gen_url, save_txt, txt_by_url
 from model.account import account_new
+from _route import route
 
-
-class ViewIndex(View):
-    def get(self, url):
-        print self.user_id
-        if not url:
-            url = gen_url()
-            self.redirect(url)
-        else:
-            self.render('/index.html', txt=txt_by_url(url), url=url)
-
-    def post(self, url):
-        if url:
-            url = url.lower()
-            txt = self.get_argument('txt', '').rstrip()
-            save_txt(url, txt)
-        self.finish({'time':int(time.time())})
 
 class SignIndex(View):
     def get(self):
@@ -47,3 +32,20 @@ class GoogleHandler(tornado.web.RequestHandler, tornado.auth.GoogleMixin):
             user_id = account_new(name, email)
             login(self, user_id)
             self.redirect('/')
+
+@route("/(.*)")
+class ViewIndex(View):
+    def get(self, url):
+        print self.user_id
+        if not url:
+            url = gen_url()
+            self.redirect(url)
+        else:
+            self.render('/index.html', txt=txt_by_url(url), url=url)
+
+    def post(self, url):
+        if url:
+            url = url.lower()
+            txt = self.get_argument('txt', '').rstrip()
+            save_txt(url, txt)
+        self.finish({'time':int(time.time())})
