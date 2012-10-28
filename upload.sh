63df32192b33cdbd0511c5f68473af3dea0428ea
MichaelGe <geyuanye@gmail.com>
2012-10-28 18:15:46 +0800

    f

--- upload.sh
+++ upload.sh
@@ -2,7 +2,6 @@ PREFIX=$(cd "$(dirname "$0")"; pwd)
 
 jitter $PREFIX/coffee $PREFIX/js &
 python $PREFIX/css_js.py
-svn add $PREFIX/build/*.css 
-svn add $PREFIX/build/*.js 
+svn add $PREFIX/* --force
 svn ci -m f
 ps x -u $USER|ack 'jitter'|awk  '{print $1}'|xargs kill -9 > /dev/null 2>&1
