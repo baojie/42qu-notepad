952b04e4f397ae6c55d51e082e5fc48a94c09adf
zcx <zzccxx@163.com>
2012-10-28 16:52:18 +0800

    f

--- js/history.js
+++ js/history.js
@@ -55,4 +55,9 @@
 
   page_history(location.hash.slice(2));
 
+  if (!$('#note-list').children().length) {
+    $('.paging').html('尚无内容 , <a href="/">点此新建</a>');
+    $('#note_list_loading').css('background', 'none');
+  }
+
 }).call(this);
