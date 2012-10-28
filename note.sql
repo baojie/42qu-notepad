dea27c6142ec463f10730330b44b50cf0b71845c
MichaelGe <geyuanye@gmail.com>
2012-10-28 13:10:38 +0800

    f

--- note.sql
+++ note.sql
@@ -1,14 +1,12 @@
-DROP DATABASE IF EXISTS `work_notepad`;
-CREATE DATABASE IF NOT EXISTS `work_notepad`;
-DROP TABLE IF EXISTS `work_notepad`.`account`;
-CREATE TABLE  `work_notepad`.`account` (
+DROP TABLE IF EXISTS `account`;
+CREATE TABLE  `account` (
   `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
   `name` varchar(32) COLLATE utf8_bin NOT NULL,
   `email` varchar(128) COLLATE utf8_bin NOT NULL,
-  PRIMARY KEY (`user_id`)
+  PRIMARY KEY (`id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
-DROP TABLE IF EXISTS `work_notepad`.`user_note`;
-CREATE TABLE  `work_notepad`.`user_note` (
+DROP TABLE IF EXISTS `user_note`;
+CREATE TABLE  `user_note` (
   `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
   `user_id` bigint(20) unsigned NOT NULL,
   `url_id` bigint(20) unsigned NOT NULL,
@@ -18,20 +16,20 @@ CREATE TABLE  `work_notepad`.`user_note` (
   UNIQUE KEY `url_id` (`url_id`),
   KEY `user_id-view_time` (`user_id`,`view_time`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
-DROP TABLE IF EXISTS `work_notepad`.`url`;
-CREATE TABLE  `work_notepad`.`url` (
+DROP TABLE IF EXISTS `url`;
+CREATE TABLE  `url` (
   `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
   `url` varbinary(512) NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `url` (`url`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
-DROP TABLE IF EXISTS `work_notepad`.`txt_log`;
-CREATE TABLE  `work_notepad`.`txt_log` (
+DROP TABLE IF EXISTS `txt_log`;
+CREATE TABLE  `txt_log` (
   `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
   `url_id` bigint(20) unsigned NOT NULL,
   `user_id` bigint(20) unsigned NOT NULL,
   `time` bigint(20) unsigned NOT NULL,
   PRIMARY KEY (`id`),
-  KEY `url_id-time` (`url_id`,`time`)
+  KEY `url_id-time` (`url_id`,`time`),
   KEY `user_id-time` (`user_id`,`time`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
