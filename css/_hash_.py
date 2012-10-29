3ae1b91bf72f89bbf7f0d75c891fe6861c9d578b
MichaelGe <geyuanye@gmail.com>
2012-10-29 11:34:06 +0800

    f

--- css/_hash_.py
+++ css/_hash_.py
@@ -4,7 +4,7 @@ import _env
 
 __HASH__ =  {
     "base.css" : 'dEnughKoGCBkAXRcphq2pg.css', #base
-    "history.css" : 'OYjOjx-UCcLJCFnOWMPNlQ.css', #history
+    "history.css" : '9Y1MDtH2O0fPmpz4lsGFlg.css', #history
     "reset.css" : '4yUW2pgRTE8SngskVbqJRQ.css', #reset
     "index.css" : 'vEWFegO-5PjMkjQxyUFdhQ.css', #index
 }
